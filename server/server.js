007376cd0b1df88cbf359d7a869a185d194d705c
PotatoInTheBox <ivangusachenko3@gmail.com>
2024-11-30 04:39:59 -0700

    implement https

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const exp = require("constants");
 const express = require("express");
+const https = require('https');
+const fs = require('fs');
 const app  = express();
 const port = process.env.PORT || 3000;
 const host = "localhost";
 // const host = "0.0.0.0"
+
+// Load the self-signed certificate and private key
+const sslOptions = {
+    key: fs.readFileSync('server/server.key'),
+    cert: fs.readFileSync('server/server.cert')
+};
 
 app.use(express.static('public'))
 app.use(express.json())
@@ -17,5 +25,9 @@ const authRoutes = require('./routes/auth');
 app.use('/game', gameRoutes);
 app.use('/auth', authRoutes);
 
-app.listen(port,host, () =>
- console.log(`Example app listening at http://${host}:${port}`))
+https.createServer(sslOptions, app).listen(port, host, () => {
+    console.log(`Example app listening at https://${host}:${port}`);
+});
+
+// app.listen(port,host, () =>
+//  console.log(`Example app listening at http://${host}:${port}`))
