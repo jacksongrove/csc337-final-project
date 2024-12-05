f84098cb13102ba4851105d45de80676c6e004a7
PotatoInTheBox <ivangusachenko3@gmail.com>
2024-12-05 12:07:35 -0700

    add back login/signup but don't include password

--- server/server.js
+++ server/server.js
@@ -12,11 +12,13 @@ app.use(express.urlencoded({ extended: true }));
 
 // Import routes
 const gameRoutes = require('./routes/game');
+const authRoutes = require('./routes/auth');
 const db = require('./db');
 
 
 // Use routes
 app.use('/game', gameRoutes);
+app.use('/auth', authRoutes);
 
 app.listen(port,host, () =>
     console.log(`Example app listening at http://${host}:${port}`))
